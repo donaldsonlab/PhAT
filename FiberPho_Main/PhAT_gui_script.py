66fd7b72301209bc04731419ee818249b6d88b53
kamu7656 <60203249+kamu7656@users.noreply.github.com>
2023-03-26 18:49:03 -0600

    Update PhAT_gui_script.py
    Explicitly declaring the default port 5006 to be used

--- FiberPho_Main/PhAT_gui_script.py
+++ FiberPho_Main/PhAT_gui_script.py
@@ -22,7 +22,7 @@ import FiberClass as fc
 
 '''
 Command to run script:
-    panel serve --show PhAT_gui_script.py --websocket-max-message-size=104876000 --autoreload
+    panel serve --show PhAT_gui_script.py --websocket-max-message-size=104876000 --autoreload --port 5006
 '''
 
 #current obj version
